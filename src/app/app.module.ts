7a876032b0b6711e2ba9886811c4f79f0fd9b58d
MonMtz <monse@vkapp.mx>
2019-05-27 17:16:50 -0500

    adding all

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
+import {APP_ROUTING} from './app.routes'
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AsideComponent } from './components/shared/aside/aside.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AsideComponent,
+    HeaderComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
